fb7c86e4f0a702df91b6472bff03f6154947ad8b
LucasBarragan <lucasbarragan000@gmail.com>
2026-04-08 18:52:20 -0300

    Agrego comentario
    El profe arregla algo, yo solo lo modifico para hacer un commit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ print("Hola Mundo!")
 
 nombre = input("Ingrese su nombre: ")
 
-print(f"Hola {nombre}")
+print(f"Hola {nombre}") #Pensé que el profe se había equivocado y lo escribí bien desde un principio, jeje
